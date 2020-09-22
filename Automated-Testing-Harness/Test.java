e1a1ca8fb8a34cb83f540c72bdbaa559006062c4
Rakesh Bandi <rbandi@ucsc.edu>
2020-09-22 16:42:56 -0700

    v2

--- Automated-Testing-Harness/Test.java
+++ Automated-Testing-Harness/Test.java
@@ -4,7 +4,7 @@ class Test{
    public static int double_val(int a, int b, int c, int d){
       return 2*a;
    }
-   public double kings(String b, int ack){
+   public double kings(double b, int ack){
       return 0;
    }
    String vodafone(int b){return "hello";}
