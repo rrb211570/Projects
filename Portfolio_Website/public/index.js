58d2826d8458e1a5a85be74fd454fd0b460c45f7
Rakesh Bandi <rbandi@ucsc.edu>
2020-09-23 14:17:19 -0700

    Portfolio Website

---
+++ Portfolio_Website/public/index.js
@@ -0,0 +1,29 @@
+document.getElementById("proj2").style.display = "none";
+document.getElementById("proj3").style.display = "none";
+document.getElementById("proj4").style.display = "none";
+function myFunction() {
+   document.getElementById("proj1").style.display = "none";
+   document.getElementById("proj2").style.display = "none";
+   document.getElementById("proj3").style.display = "none";
+   document.getElementById("proj4").style.display = "none";
+}
+function myFunction1() {
+   document.getElementById("proj1").style.display = "block";
+   document.getElementById("proj_links").style.background = "url(button2.jpg)";
+   document.getElementById("proj_links").style.backgroundSize = "cover";
+}
+function myFunction2() {
+   document.getElementById("proj2").style.display = "block";
+   document.getElementById("proj_links").style.background = "url(button2.jpg)";
+   document.getElementById("proj_links").style.backgroundSize = "cover";
+}
+function myFunction3() {
+   document.getElementById("proj3").style.display = "block";
+   document.getElementById("proj_links").style.background = "url(button2.jpg)";
+   document.getElementById("proj_links").style.backgroundSize = "cover";
+}
+function myFunction4() {
+   document.getElementById("proj4").style.display = "block";
+   document.getElementById("proj_links").style.background = "url(button2.jpg)";
+   document.getElementById("proj_links").style.backgroundSize = "cover";
+}
