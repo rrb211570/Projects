c28f7304734698c429b3921236295e56ecbed0f0
Rakesh Bandi <rbandi@ucsc.edu>
2020-09-24 15:57:15 -0700

    Parses map.txt v1

--- Automated-Testing-Harness/Harness.pl
+++ Automated-Testing-Harness/Harness.pl
@@ -3,8 +3,8 @@ use warnings;
 use strict;
 
 # Helpers
-sub stressParse {
-   my ($in) = @_;
+sub parse {
+   my ($in, $commandType) = @_;
    open(my $infile, $in) or die "Could not open file: '$in'";
    my @in_name = split(/.java$/, $in); # class name
 
@@ -20,12 +20,12 @@ sub stressParse {
             my @tmp3 = split(/\s*\,\s*/,$tmp2[0]);
             foreach(@tmp3) {
                my @tmp4 = split(/\s+/,$_);
-               if($tmp4[0] ne 'int') {$valid = 0; }
+               if($tmp4[0] ne 'int' && $commandType == 0) {$valid = 0; } # toggle
                push(@functionDef, split(/\s+/,$tmp4[0]));
             }
          } else {
             my @tmp4 = split(/\s+/,$tmp2[0]);
-            if($tmp4[0] ne 'int') {$valid = 0; }
+            if($tmp4[0] ne 'int' && $commandType == 0) {$valid = 0; } # toggle
             push(@functionDef,$tmp4[0]);
          }
          if($valid == 1) {
@@ -37,19 +37,68 @@ sub stressParse {
    return \@functionList;
 }
 
+sub parseMap {
+   my ($map) = @_;
+   open(my $infile, $map) or die "Could not open file: '$map'";
+
+   # parse
+   my @mapFunctionList;
+   my $state = 0;
+   my @line;
+   my $head;
+   my @in;
+   my $out;
+   print "\nMapping data:\n\n";
+   while(<$infile>) {
+      if($state == 0) {
+         if(/^\s*(\S+)\s*$/) {
+            $head = $1;
+            $state = 1;
+         } else {
+            die "Invalid format in mapping file:\n\n$_\n\n";
+         }
+      } elsif($state == 1) {
+         if(/^\s*\( \s*( (\S+\s*\,\s*)* (\s*\S+) )\s* \)\s*$/x) {
+            my $input_line = $1;
+            # split up into array;
+            if($input_line =~ /^.*\,.*$/){
+               @in = split(/\s*\,\s*/,$input_line);
+            } else {
+               /^\s*(\S+)\s*$/;
+               push(@in, $1);
+            }
+            $state = 2;
+         } else {
+            die "Invalid format in mapping file:\n\n$_\n\n";
+         }
+      } elsif($state == 2) {
+         if(/^\s*(\S+)\s*$/) {
+            $out = $1;
+            push(@line,$head);
+            foreach(@in) {
+               push(@line, $_);
+            }
+            push(@line, $out);
+            foreach(@line){
+               print "'$_' ";
+            }
+            print "\n";
+            $state = 0;
+            @in = ();
+            @line = ();
+         } else {
+            die "Invalid format in mapping file:\n\n$_\n\n";
+         }
+      }
+   }
+   return \@mapFunctionList;
+}
+
 sub makeStress {
    my ($functionList, $write_name) = @_;
-   foreach(@$functionList) {
-      my @arr = @$_;
-      print "Testing function: ";
-      foreach(@arr) {
-         print("'$_' ");
-      }
-      print "\n";
-   }
-
    open(my $outfile, ">$write_name") or die "Could not open file";
    my @write_class = split(/.java/,$write_name);
+
    # populate .java stress test
    print $outfile "//Integer Stress Test\n\n";
    print $outfile "class $write_class[0] {\n";
@@ -65,6 +114,18 @@ sub makeStress {
    }
    print $outfile "   }\n}";
    close($outfile) or die "Could not close";
+}
+
+sub printFunction {
+   my ($functionList) = @_;
+   foreach(@$functionList) {
+      my @arr = @$_;
+      print "Testing function: ";
+      foreach(@arr) {
+         print("'$_' ");
+      }
+      print "\n";
+   }
 }
 
 # Script Start
@@ -83,7 +144,6 @@ if($#ARGV + 1 == 3) {
 if($commandType == -1) {
    die "\nErr: Correct form:\n\nInt Stress Test: Harness.pl [input file] [output file name]\nIO Mapping Test: Harness.pl [input file] [output file name] [mapping file]\n\nFlags: '-v' : verbose\n";
 }
-
 $in = shift(@ARGV);
 $out = shift(@ARGV);
 
@@ -109,11 +169,18 @@ if($commandType == 1) {
 print "\n";
 
 # process
+my $functionList = parse($in, $commandType);
+printFunction($functionList);
+
 if($commandType == 0){
-   my $functionList = stressParse($in);
    makeStress($functionList, $out);
 }
 if($commandType == 1){
-   print "mapping\n";
+   # ------ make sure IO mapping corresponds with function IO
+   my $mapFunctionList = parseMap($map);
+   # trimList($functionList, $mapFunctionList);
+
+   # ------ construct Hashmap in 'out.java', and test mapping data
+   # makeMap($functionList, $map, $out);
 }
 print "\nComplete\n";
